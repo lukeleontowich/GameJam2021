bb75647140c0f4c4eda4259c009bf184f1e3c70a
Luke Leontowich <luke.leontowich@uleth.ca>
2021-06-11 17:47:38 -0600

    Blueblob moving

---
+++ GameJam2021/src/Rope.lua
@@ -0,0 +1,19 @@
+--  rope.lua
+
+Rope = class{__includes Entity}
+
+function Rope:init(param) 
+    self.redBlob = param.redBlob
+    self.blueBlob = param.blueBlob
+    
+end
+
+function Rope:update(dt)
+    self.length = (
+        (self.redBlob.x - self.blueBlob.x) *
+        (self.redBlob.x - self.blueBlob.x) +
+        (self.rebBlob.y - self.blueBlob.y) *
+        (self.redBlob.y - self.blueBlob.y))
+    
+    print("rope lenght:", length)
+end
