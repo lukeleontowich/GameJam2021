d9f48df24775bc15cc58c3a325ee5720d20f6e87
Luke Leontowich <luke.leontowich@uleth.ca>
2021-06-11 16:03:52 -0600

    Fixed issue

--- GameJam2021/src/states/StartState.lua
+++ GameJam2021/src/states/StartState.lua
@@ -16,14 +16,12 @@ end
 function StartState:render()
 
 
-    love.graphics.draw(gTextures['background'], 0, 0)
-    love.graphics.draw(gTextures['red_blob_still'], (VIRTUAL_WIDTH / 2) - 16, VIRTUAL_HEIGHT - 32)
-    love.graphics.draw(gTextures['tile'], 0, 0)
+    
     love.graphics.setColor(0.1, 1.0, 0.1, 1.0)
     love.graphics.printf("Welcome!", 
     0, VIRTUAL_HEIGHT / 2 - 48, VIRTUAL_WIDTH, 'center')
     love.graphics.printf("Press Any Key to Continue", 0, VIRTUAL_HEIGHT / 2 - 24, VIRTUAL_WIDTH, 'center')
-    love.graphics.setColor(self.color)
+    love.graphics.setColor({1.0, 1.0, 1.0, 1.0})
     love.graphics.draw(gTextures['background'], 0, 0)
     love.graphics.setFont(gFonts['title'])
     love.graphics.printf('Blobs', 1, VIRTUAL_HEIGHT / 2 - 40 + 1, VIRTUAL_WIDTH, 'center')
