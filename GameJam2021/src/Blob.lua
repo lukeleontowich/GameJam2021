6bada95cd27eb815d362e41a76c17636c5b5ac6f
mcdj3780 <jenna.mcdonnell@uleth.ca>
2021-06-11 15:55:25 -0600

    changes to start screen

---
+++ GameJam2021/src/Blob.lua
@@ -0,0 +1,21 @@
+Blob = Class{}
+
+function Blob:init(params)
+    self.x = params.x
+    self.y = params.y
+
+    self.color = params.color
+
+    self.currentAnimation = 1
+end
+
+function Blob:update(dt)
+end
+
+function Blob:render()
+    if self.color == 1 then
+        love.graphics.draw(gTextures['red_blob_still'], self.x, self.y)
+    else
+        love.graphics.draw(gTextures['blue_blob_still'], self.x, self.y)
+    end
+end
--- GameJam2021/src/states/StartState.lua
+++ GameJam2021/src/states/StartState.lua
@@ -1,7 +1,22 @@
 StartState = Class{__includes = BaseState}
 
+function StartState:init()
+    self.blueBlob = Blob({
+        x = (VIRTUAL_WIDTH / 2) - 48, 
+        y = VIRTUAL_HEIGHT - 32,
+        color = 2
+    })
+    self.redBlob = Blob({
+        x = (VIRTUAL_WIDTH / 2) + 16, 
+        y = VIRTUAL_HEIGHT - 32,
+        color = 1
+    })
+end
+
 function StartState:render()
     love.graphics.draw(gTextures['background'], 0, 0)
-    love.graphics.draw(gTextures['red_blob_still'], (VIRTUAL_WIDTH / 2) - 16, VIRTUAL_HEIGHT - 32)
-    love.graphics.draw(gTextures['tile'], 0, 0)
+    love.graphics.setFont(gFonts['title'])
+    love.graphics.printf('Blobs', 1, VIRTUAL_HEIGHT / 2 - 40 + 1, VIRTUAL_WIDTH, 'center')
+    self.blueBlob:render()
+    self.redBlob:render()
 end
