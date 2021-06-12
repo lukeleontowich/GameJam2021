6c4a890322fd96511aa2415ceafb1da2f9a2c762
Luke Leontowich <luke.leontowich@uleth.ca>
2021-06-11 20:41:27 -0600

    working on rope

--- GameJam2021/src/Rope.lua
+++ GameJam2021/src/Rope.lua
@@ -5,69 +5,59 @@ Rope = Class{__includes = Entity}
 function Rope:init(param) 
     self.redBlob = param.redBlob
     self.blueBlob = param.blueBlob
-    self.max_length = (VIRTUAL_WIDTH / 2) * (VIRTUAL_WIDTH / 2)
+    self.max_length = (VIRTUAL_WIDTH / 1.5) * (VIRTUAL_WIDTH / 1.5)
     --  80% the max_lenth
-    self.tensor_length = self.max_length * 0.8 
+    self.tensor_length = self.max_length * 0.5
     self.tensor_num  = 1.0
     self.original_speed = self.redBlob.dx
     self.length = 0
     self.center_x = 0
     self.center_y = 0
+    self.tense = false
 end
---[[
-function Rope:getCenter()
-    local x1, x2, y1, y2 = 0
-    if self.redBlob.x > self.blueBlob.x and self.redBlob.y > self.blueBlob.y then
-        x1 = self.blueBlob.x
-        x2 = self.redBlob.x
-    else
-        x1 = self.redBlob.x
-        x2 = self.blueBlob.x
-    end
-    --if  then
-        y1 = self.blueBlob.y
-        y2 = self.redBlob.y
-   -- else
-        y1 = self.redBlob.y
-        y2 = self.blueBlob.y
-    end
-    self.center_x = (x2 - x1) / 2
-    self.center_y = (y2 - y1) / 2
 
-    --self.redBlob.x = 
+function Rope:ropeSnap()
+    local center_x = 0
+    local center_y = 0
+    --  case 1
+    if self.redBlob.x < self.blueBlob.x and self.redBlob.y > self.blueBlob.y then
+        center_x = (self.blueBlob.x - self.redBlob.x) / 2 + self.redBlob.x
+        center_y = (self.redBlob.y - self.blueBlob.y) / 2 + self.blueBlob.y 
+        self.redBlob.x = center_x - self.redBlob.width
+        self.redBlob.y = center_y + self.redBlob.height
+        self.blueBlob.x = center_x + self.blueBlob.width
+        self.blueBlob.y = center_y + self.redBlob.height
+    -- case 2
+    elseif self.redBlob.x > self.blueBlob.x and self.redBlob.y < self.blueBlob.y then
+        center_x = (self.redBlob.x - self.blueBlob.x) / 2 + self.blueBlob.x
+        center_y = (self.blueBlob.y - self.redBlob.y) / 2 + self.redBlob.y 
+        self.redBlob.x = center_x - self.redBlob.width
+        self.redBlob.y = center_y - self.redBlob.height
+        self.blueBlob.x = center_x + self.blueBlob.width
+        self.blueBlob.y = center_y + self.blueBlob.height
+    end
+
+
 end
-]]
+
 function Rope:update(dt)
-    print("printing", self.redBlob.width)
+   
     self.length = (
         (self.redBlob.x - self.blueBlob.x) *
         (self.redBlob.x - self.blueBlob.x) +
         (self.redBlob.y - self.blueBlob.y) *
         (self.redBlob.y - self.blueBlob.y))
     if self.length > self.max_length then
-        local center_x = (self.redBlob.x - self.blueBlob.x) / 2
-        local center_y = (math.abs(self.redBlob.y - self.blueBlob.y)) / 2
-        if self.redBlob.x > self.blueBlob.x then
-            self.redBlob.x = center_x + 8
-            self.blueBlob.x = center_x - 8
-        else
-            self.redBlob.x = center_x - 8
-            self.blueBlob.x = center_x + 8
-        end
-        if self.redBlob.y > self.blueBlob.y then
-            self.redBlob.y = center_y + 8
-            self.blueBlob.y = center_y - 8
-        else
-            self.redBlob.y = center_y - 8
-            self.blueBlob.y = center_y + 8
-        end
-    end
-    if self.length > self.tensor_length then
-        self.redBlob.dx = self.original_speed * 0.2
-        self.redBlob.dy = self.original_speed * 0.2
-        self.blueBlob.dx = self.original_speed * 0.2
-        self.blueBlob.dy = self.original_speed * 0.2
+        --self:ropeSnap()
+    
+    elseif self.length > self.tensor_length then
+        self.tensor_num = 1.0 - self.length / self.max_length 
+        self.redBlob.dx = self.original_speed * self.tensor_num
+        self.redBlob.dy = self.original_speed * self.tensor_num
+        self.blueBlob.dx = self.original_speed * self.tensor_num
+        self.blueBlob.dy = self.original_speed * self.tensor_num
     else
+        self.tensor_num = 1.0
         self.redBlob.dx = self.original_speed 
         self.redBlob.dy = self.original_speed 
         self.blueBlob.dx = self.original_speed
@@ -77,11 +67,13 @@ end
 
 function Rope:render()
     local color = {1.0, 1.0, 1.0, 1.0}
-    love.graphics.setColor(0.7, 0.7, 0.7, 1.0)
+    love.graphics.setColor(1- self.tensor_num, self.tensor_num, 0.0, 1.0)
     love.graphics.line(
         self.redBlob.x + self.redBlob.width, 
         self.redBlob.y + self.redBlob.height, 
         self.blueBlob.x + self.blueBlob.width , 
-        self.blueBlob.y + self.blueBlob.height)
+        self.blueBlob.y + self.blueBlob.height
+    )
+    love.graphics.setColor(1.0, 1.0, 1.0, 1.0)
 
 end
