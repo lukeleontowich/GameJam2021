dff61cad27cf0ea1bafac3f53b9625ac2b3ffbc0
Luke Leontowich <luke.leontowich@uleth.ca>
2021-06-11 18:39:40 -0600

    Roping good time

--- GameJam2021/src/states/PlayState.lua
+++ GameJam2021/src/states/PlayState.lua
@@ -1,26 +1,29 @@
 PlayState = Class{__includes = BaseState}
 
 function PlayState:init()
-    self.rope = Rope({
-        blueBlob = self.blueBlob,
-        redBlob = self.redBlob
-    })
+
 end
 
 function PlayState:update(dt)
     self.redBlob:update(dt)
     self.blueBlob:update(dt)
+    self.rope:update(dt)
 end
 
 function PlayState:render()
     love.graphics.draw(gTextures['background'], 0, 0)
     self.redBlob:render()
     self.blueBlob:render()
+    self.rope:render()
 end
 
 function PlayState:enter(params)
     self.redBlob = params.redBlob
     self.blueBlob = params.blueBlob
     self.blueBlob.x = 0
-    self.redBlob.x = 32
+    self.redBlob.x = 32    
+    self.rope = Rope({
+        blueBlob = self.blueBlob,
+        redBlob = self.redBlob
+    })
 end
