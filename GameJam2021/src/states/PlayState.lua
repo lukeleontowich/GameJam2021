d9408e188316fc991af544cf8559f86d485b01c7
Luke Leontowich <luke.leontowich@uleth.ca>
2021-06-12 18:28:36 -0600

    Working on levels

--- GameJam2021/src/states/PlayState.lua
+++ GameJam2021/src/states/PlayState.lua
@@ -1,22 +1,13 @@
 PlayState = Class{__includes = BaseState}
 
+
 function PlayState:init()
     self.tiles = LevelMaker.generate(VIRTUAL_WIDTH, VIRTUAL_HEIGHT) 
-    self.chest_key = ChestKey( 
-        {chest = Chest({x = VIRTUAL_WIDTH / 4, y = VIRTUAL_HEIGHT / 4}),
-        key = Key({x = 3 * VIRTUAL_WIDTH / 4, y = 3 * VIRTUAL_HEIGHT / 4})}
-    )
     self.portal = Portal({
         left_x = 20,
         left_y = 20,
         right_x = VIRTUAL_WIDTH - 40,
         right_y = 20
-    })
-
-    self.pressure_button = Button({
-        x = 0, 
-        y = VIRTUAL_HEIGHT - 14, 
-        type = 1 
     })
 
     self.arrow_button = Button({
@@ -26,101 +17,101 @@ function PlayState:init()
     })
 
     self.health = 3
-
-    self.pbutton1 = PressureButton({y = 20, side = 1})
-    self.pbutton2 = PressureButton({y = 50, side = 2})
 end
 
 function PlayState:update(dt)
+    --  Update the Blobs
     self.redBlob:update(dt)
     self.blueBlob:update(dt)
+
+    --  Get the velocity of the rope (needed for the push buttons)
     local vel_left, vel_right = self.rope:update(dt)
-    --  TAKE THIS OUT LATER
-    if vel_left > 0 then
-        print("vel_left: ", vel_left)
-    end
-    if vel_right > 0 then
-        print("vel_right: ", vel_right)
+
+    --  update the pressure buttons
+    for button in pairs(self.level.pressure_buttons) do 
+        self.level.pressure_buttons[button]:update(vel_left, vel_right, self.redBlob, self.blueBlob)
     end
 
-    self.pbutton1:update(vel_left, vel_right, self.redBlob, self.blueBlob)
-    self.pbutton2:update(vel_left, vel_right, self.redBlob, self.blueBlob)
-    for x in pairs(self.enemies) do
-        self.enemies[x]:update(dt)
+    
+    --  Update Enemy positions
+    for x in pairs(self.level.enemies) do
+        self.level.enemies[x]:update(dt)
     end
 
     --check for enemy collisions
-    for enemy in pairs(self.enemies) do
-        if self.enemies[enemy]:collides(self.redBlob) and not self.enemies[enemy].isDead then
+    for enemy in pairs(self.level.enemies) do
+        if self.level.enemies[enemy]:collides(self.redBlob) and not self.level.enemies[enemy].isDead then
             gSounds['enemy_death']:play()
-            self.enemies[enemy].isDead = true
-            self.enemies[enemy].timer = 0
-        elseif self.enemies[enemy]:hurt(self.redBlob) then
+            self.level.enemies[enemy].isDead = true
+            self.level.enemies[enemy].timer = 0
+        elseif self.level.enemies[enemy]:hurt(self.redBlob) then
             gSounds['hurt']:play()
             self.health = self.health - 1
         end
     end
 
-    for enemy in pairs(self.enemies) do
-        if self.enemies[enemy]:collides(self.blueBlob) and not self.enemies[enemy].isDead then
+    for enemy in pairs(self.level.enemies) do
+        if self.level.enemies[enemy]:collides(self.blueBlob) and not self.level.enemies[enemy].isDead then
             gSounds['enemy_death']:play()
-            self.enemies[enemy].isDead = true
-            self.enemies[enemy].timer = 0
-        elseif self.enemies[enemy]:hurt(self.blueBlob) then
+            self.level.enemies[enemy].isDead = true
+            self.level.enemies[enemy].timer = 0
+        elseif self.level.enemies[enemy]:hurt(self.blueBlob) then
             gSounds['hurt']:play()
             self.health = self.health - 1
         end
-        if not self.enemies[enemy].exists then
-            table.remove(self.enemies, enemy)
+        if not self.level.enemies[enemy].exists then
+            table.remove(self.level.enemies, enemy)
         end
     end
 
-    for enemy in pairs(self.enemies) do
-        for enemy2 in pairs(self.enemies) do
-            if self.enemies[enemy].x < self.enemies[enemy2].x + self.enemies[enemy2].width 
-            and self.enemies[enemy].x > self.enemies[enemy2].x then
-                self.enemies[enemy].x = self.enemies[enemy].x + self.enemies[enemy].dx
-            elseif self.enemies[enemy].y < self.enemies[enemy2].y + self.enemies[enemy2].height
-            and self.enemies[enemy].y > self.enemies[enemy2].y then
-                self.enemies[enemy].y = self.enemies[enemy].y + self.enemies[enemy].dy
+    for enemy in pairs(self.level.enemies) do
+        for enemy2 in pairs(self.level.enemies) do
+            if self.level.enemies[enemy].x < self.level.enemies[enemy2].x + self.level.enemies[enemy2].width 
+            and self.level.enemies[enemy].x > self.level.enemies[enemy2].x then
+                self.level.enemies[enemy].x = self.level.enemies[enemy].x + self.level.enemies[enemy].dx
+            elseif self.level.enemies[enemy].y < self.level.enemies[enemy2].y + self.level.enemies[enemy2].height
+            and self.level.enemies[enemy].y > self.level.enemies[enemy2].y then
+                self.level.enemies[enemy].y = self.level.enemies[enemy].y + self.level.enemies[enemy].dy
             end
         end
     end
 
-    --  Check if chest is opened and if the blobs have the key
-    if not self.chest_key:isOpened() then
-        if self.chest_key:hasKey(self.blueBlob) then
-            self.chest_key:openingChest(self.blueBlob)
-        elseif self.chest_key:hasKey(self.redBlob) then
-            self.chest_key:openingChest(self.redBlob)
+    --  Check if chests are opened and if the blobs have the key
+    for x in pairs(self.level.chest_keys) do
+        if not self.level.chest_keys[x]:isOpened() then
+            if self.level.chest_keys[x]:hasKey(self.blueBlob) then
+                self.level.chest_keys[x]:openingChest(self.blueBlob)
+            elseif self.level.chest_keys[x]:hasKey(self.redBlob) then
+                self.level.chest_keys[x]:openingChest(self.redBlob)
+            end
         end
     end
 
     --  See if the key is in the portal and if so send it
-    if self.portal:collides(self.chest_key.key) and not self.chest_key.portaled then
-        self.blueBlob.has_key = false
-        self.redBlob.has_key = false
+    --  There will be an error rn for muliple cchest_keys
+    for x in pairs(self.level.chest_keys) do
+        if self.portal:collides(self.level.chest_keys[x].key) and not self.level.chest_keys[x].portaled then
+            self.blueBlob.has_key = false
+            self.redBlob.has_key = false
+        end
     end
 
     --  Check if an enemy is in a portal then send it
-    for enemy in pairs(self.enemies) do
-        if not self.enemies[enemy].portaled then 
-            if self.portal:collides(self.enemies[enemy]) then
-                self.enemies[enemy].portaled = true
-                self.enemies[enemy].timer = 0
-                if self.enemies[enemy].side == 1 then
+    for enemy in pairs(self.level.enemies) do
+        if not self.level.enemies[enemy].portaled then 
+            if self.portal:collides(self.level.enemies[enemy]) then
+                self.level.enemies[enemy].portaled = true
+                self.level.enemies[enemy].timer = 0
+                if self.level.enemies[enemy].side == 1 then
                     print("here")
-                    self.enemies[enemy].side = 2 
-                    print("enemy side:", self.enemies[enemy].side)
-                elseif self.enemies[enemy].side == 2 then
+                    self.level.enemies[enemy].side = 2 
+                    print("enemy side:", self.level.enemies[enemy].side)
+                elseif self.level.enemies[enemy].side == 2 then
                     print("shoudln't be here")
-                    self.enemies[enemy].side = 1
+                    self.level.enemies[enemy].side = 1
                 end
             end
         end
-    end
-    for enemy in pairs(self.enemies) do
-        --print("e1 side: ", self.enemies[enemy].side)
     end
 
     if self.arrow_button:collides(self.redBlob) or self.arrow_button:collides(self.blueBlob) then
@@ -147,29 +138,48 @@ function PlayState:update(dt)
 end
 
 function PlayState:render()
+    --  render backgrou9ns
     love.graphics.draw(gTextures['background'], 0, 0)
     for tile in pairs(self.tiles) do
         self.tiles[tile]:render()
     end
+
+    -- render hearts
     local distance = VIRTUAL_WIDTH - 9
     for h = 1, self.health do
         love.graphics.draw(gTextures['heart'], distance, 0)
         distance = distance - 9
     end
+
     self.portal:render()
-    self.pressure_button:render()
-    self.pbutton1:render()
-    self.pbutton2:render()
+
+    --  render pressure buttons
+    for x in pairs(self.level.pressure_buttons) do
+        self.level.pressure_buttons[x]:render()
+    end
+
+
     self.arrow_button:render()
-    self.chest_key.chest:render() 
+
+    --  render chests
+    for x in pairs(self.level.chest_keys) do
+        self.level.chest_keys[x].chest:render() 
+    end
+
+    --  render rope
     self.rope:render()
+
+    --  render blobs
     self.redBlob:render()
     self.blueBlob:render()
-    if not self.chest_key:isOpened() then
-        self.chest_key.key:render()
+    for x in pairs(self.level.chest_keys) do 
+        if not self.level.chest_keys[x]:isOpened() then
+            self.level.chest_keys[x].key:render()
+        end
     end
-    for x in pairs(self.enemies) do
-        self.enemies[x]:render()
+
+    for x in pairs(self.level.enemies) do
+        self.level.enemies[x]:render()
     end
 end
 
@@ -186,5 +196,36 @@ function PlayState:enter(params)
         blueBlob = self.blueBlob,
         redBlob = self.redBlob
     })
-    self.enemies = EnemySpawner.generate(self.redBlob, self.blueBlob)
+
+    self.level1 = {
+        chest_keys = {
+            ChestKey({
+                chest = Chest({x = VIRTUAL_WIDTH / 4, y = VIRTUAL_HEIGHT / 4}),
+                key = Key({x = 3 * VIRTUAL_WIDTH / 4, y = 3 * VIRTUAL_HEIGHT / 4})
+            })
+        },
+        portals = {
+            Portal({
+                left_x = 20,
+                left_y = 20,
+                right_x = VIRTUAL_WIDTH - 40,
+                right_y = 20
+            })
+        },
+        pressure_buttons = {
+            PressureButton({y = 20, side = 1}),
+            PressureButton({y = 50, side = 2})
+        },
+        buttons = {
+            Button({x = 30,y = VIRTUAL_HEIGHT - 14,type = 2})
+        },
+        enemies = {
+            Enemy({x = 5, y = 50, color = 1, redBlob = self.redBlob, blueBlob = self.blueBlob}),
+            Enemy({x = 30, y = 50, color = 2, redBlob = self.redBlob, blueBlob = self.blueBlob})
+        },
+        simon_says = {}
+    }
+
+    self.level = self.level1
+    --self.enemies = self.levels.enemies
 end
